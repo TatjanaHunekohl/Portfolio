f58c043de9f601b417c1118d61c33b1efc54924c
Tatjana Hunekohl <tatjana.hunekohl@gmail.com>
2022-09-07 18:19:55 +0200

    g footer component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,20 @@
+import { FooterComponent } from './footer/footer.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { MainComponent } from './main/main.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LegalNoteComponent } from './legal-note/legal-note.component';
+import { ImprintComponent } from './imprint/imprint.component';
 import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProjectsComponent } from './projects/projects.component';
+
+
 
 const routes: Routes = [
-  { path: '', component: AppComponent }
   
-];
+  { path: 'imprint' , component: ImprintComponent} ,
+  { path: 'legal-note' , component: LegalNoteComponent}
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
